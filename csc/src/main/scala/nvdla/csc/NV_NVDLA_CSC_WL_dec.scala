233d94a7dd263ed8e4ecd6233e569fc801521ad2
yifengdu <y.f.du1994@gmail.com>
2020-03-11 20:24:14 -0500

    Annotated

--- csc/src/main/scala/nvdla/csc/NV_NVDLA_CSC_WL_dec.scala
+++ csc/src/main/scala/nvdla/csc/NV_NVDLA_CSC_WL_dec.scala
@@ -1,172 +1,172 @@
- package nvdla
+//  package nvdla
 
- import chisel3._
- import chisel3.experimental._
- import chisel3.util._
+//  import chisel3._
+//  import chisel3.experimental._
+//  import chisel3.util._
 
-@chiselName
-class csc_wl_dec_goldinput_if(implicit val conf: nvdlaConfig) extends Bundle{
-    val mask = Output(UInt(conf.CSC_ATOMC.W))
-    val data = Output(UInt((conf.CSC_ATOMC*conf.CSC_BPE).W))
-    val sel = Output(UInt(conf.CSC_ATOMK.W))
-}
+// @chiselName
+// class csc_wl_dec_goldinput_if(implicit val conf: nvdlaConfig) extends Bundle{
+//     val mask = Output(UInt(conf.CSC_ATOMC.W))
+//     val data = Output(UInt((conf.CSC_ATOMC*conf.CSC_BPE).W))
+//     val sel = Output(UInt(conf.CSC_ATOMK.W))
+// }
 
-class csc_wl_dec_goldoutput_if(implicit val conf: nvdlaConfig) extends Bundle{
-    val mask = Output(UInt(conf.CSC_ATOMC.W))
-    val data = Output(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
-    val sel = Output(UInt(conf.CSC_ATOMK.W))
-}
+// class csc_wl_dec_goldoutput_if(implicit val conf: nvdlaConfig) extends Bundle{
+//     val mask = Output(UInt(conf.CSC_ATOMC.W))
+//     val data = Output(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
+//     val sel = Output(UInt(conf.CSC_ATOMK.W))
+// }
 
-class NV_NVDLA_CSC_WL_dec(implicit val conf: nvdlaConfig) extends Module {
-    val io = IO(new Bundle {
-        //clock
-        val nvdla_core_clk = Input(Clock()) 
-        val nvdla_core_rstn = Input(Bool())
+// class NV_NVDLA_CSC_WL_dec(implicit val conf: nvdlaConfig) extends Module {
+//     val io = IO(new Bundle {
+//         //clock
+//         val nvdla_core_clk = Input(Clock()) 
+//         val nvdla_core_rstn = Input(Bool())
 
-        //input 
-        val input = Flipped(ValidIO(new csc_wl_dec_goldinput_if))
-        val input_mask_en = Input(UInt(10.W))
+//         //input 
+//         val input = Flipped(ValidIO(new csc_wl_dec_goldinput_if))
+//         val input_mask_en = Input(UInt(10.W))
 
-        //output
-        val output = ValidIO(new csc_wl_dec_goldoutput_if) 
+//         //output
+//         val output = ValidIO(new csc_wl_dec_goldoutput_if) 
 
-    })
-    //     
-    //          ┌─┐       ┌─┐
-    //       ┌──┘ ┴───────┘ ┴──┐
-    //       │                 │
-    //       │       ───       │          
-    //       │  ─┬┘       └┬─  │
-    //       │                 │
-    //       │       ─┴─       │
-    //       │                 │
-    //       └───┐         ┌───┘
-    //           │         │
-    //           │         │
-    //           │         │
-    //           │         └──────────────┐
-    //           │                        │
-    //           │                        ├─┐
-    //           │                        ┌─┘    
-    //           │                        │
-    //           └─┐  ┐  ┌───────┬──┐  ┌──┘         
-    //             │ ─┤ ─┤       │ ─┤ ─┤         
-    //             └──┴──┘       └──┴──┘ 
-withClockAndReset(io.nvdla_core_clk,!io.nvdla_core_rstn){
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    // Decoder of compressed weight                                                  
-    //
-    //            data_mask             input_data     mac_sel
-    //                |                     |            |
-    //            sums_for_sel           register     register
-    //                |                     |            |
-    //                ------------------>  mux        register
-    //                                      |            |
-    //                                   output_data  output_sel
-    //
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    val input_mask_vec = Wire(Vec(conf.CSC_ATOMC, Bool()))
-    for(i <- 0 to conf.CSC_ATOMC-1){
-        input_mask_vec(i) := io.input.bits.mask(i)
-    }
-    val input_data_vec = Wire(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
-    for(i <- 0 to conf.CSC_ATOMC-1){
-        input_data_vec(i) := io.input.bits.data(conf.CSC_BPE*i+conf.CSC_BPE-1, conf.CSC_BPE*i)
-    }
-    val input_sel_vec = Wire(Vec(conf.CSC_ATOMK, Bool()))
-    for(i <- 0 to conf.CSC_ATOMK-1){
-        input_sel_vec(i) := io.input.bits.sel(i)
-    }
-    ////////////////////////////////// phase I: calculate sums for mux //////////////////////////////////
-    val input_mask_gated = Mux(io.input_mask_en(8), input_mask_vec, VecInit(Seq.fill(conf.CSC_ATOMC)(false.B)))
-    val vec_sum = Wire(MixedVec((0 to conf.CSC_ATOMC-1) map { i => UInt((log2Ceil(i+2)).W) }))
+//     })
+//     //     
+//     //          ┌─┐       ┌─┐
+//     //       ┌──┘ ┴───────┘ ┴──┐
+//     //       │                 │
+//     //       │       ───       │          
+//     //       │  ─┬┘       └┬─  │
+//     //       │                 │
+//     //       │       ─┴─       │
+//     //       │                 │
+//     //       └───┐         ┌───┘
+//     //           │         │
+//     //           │         │
+//     //           │         │
+//     //           │         └──────────────┐
+//     //           │                        │
+//     //           │                        ├─┐
+//     //           │                        ┌─┘    
+//     //           │                        │
+//     //           └─┐  ┐  ┌───────┬──┐  ┌──┘         
+//     //             │ ─┤ ─┤       │ ─┤ ─┤         
+//     //             └──┴──┘       └──┴──┘ 
+// withClockAndReset(io.nvdla_core_clk,!io.nvdla_core_rstn){
+//     /////////////////////////////////////////////////////////////////////////////////////////////
+//     // Decoder of compressed weight                                                  
+//     //
+//     //            data_mask             input_data     mac_sel
+//     //                |                     |            |
+//     //            sums_for_sel           register     register
+//     //                |                     |            |
+//     //                ------------------>  mux        register
+//     //                                      |            |
+//     //                                   output_data  output_sel
+//     //
+//     /////////////////////////////////////////////////////////////////////////////////////////////
+//     val input_mask_vec = Wire(Vec(conf.CSC_ATOMC, Bool()))
+//     for(i <- 0 to conf.CSC_ATOMC-1){
+//         input_mask_vec(i) := io.input.bits.mask(i)
+//     }
+//     val input_data_vec = Wire(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
+//     for(i <- 0 to conf.CSC_ATOMC-1){
+//         input_data_vec(i) := io.input.bits.data(conf.CSC_BPE*i+conf.CSC_BPE-1, conf.CSC_BPE*i)
+//     }
+//     val input_sel_vec = Wire(Vec(conf.CSC_ATOMK, Bool()))
+//     for(i <- 0 to conf.CSC_ATOMK-1){
+//         input_sel_vec(i) := io.input.bits.sel(i)
+//     }
+//     ////////////////////////////////// phase I: calculate sums for mux //////////////////////////////////
+//     val input_mask_gated = Mux(io.input_mask_en(8), input_mask_vec, VecInit(Seq.fill(conf.CSC_ATOMC)(false.B)))
+//     val vec_sum = Wire(MixedVec((0 to conf.CSC_ATOMC-1) map { i => UInt((log2Ceil(i+2)).W) }))
 
-    for(i <- 0 to conf.CSC_ATOMC-1){     
-        vec_sum(i) := PopCount(Cat(input_mask_gated.asUInt)(i, 0))        
-    }  
+//     for(i <- 0 to conf.CSC_ATOMC-1){     
+//         vec_sum(i) := PopCount(Cat(input_mask_gated.asUInt)(i, 0))        
+//     }  
 
-    ////////////////////////////////// phase I: registers //////////////////////////////////
-    val valid_d1 = RegInit(false.B)
-    val data_d1 = Reg(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
-    val mask_d1 = Reg(Vec(conf.CSC_ATOMC, Bool()))
-    val sel_d1 = RegInit(VecInit(Seq.fill(conf.CSC_ATOMK)(false.B)))
-    val vec_sum_d1 = RegInit(MixedVecInit((0 to conf.CSC_ATOMC-1) map { i => Fill(log2Ceil(i+2), false.B) }))
+//     ////////////////////////////////// phase I: registers //////////////////////////////////
+//     val valid_d1 = RegInit(false.B)
+//     val data_d1 = Reg(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
+//     val mask_d1 = Reg(Vec(conf.CSC_ATOMC, Bool()))
+//     val sel_d1 = RegInit(VecInit(Seq.fill(conf.CSC_ATOMK)(false.B)))
+//     val vec_sum_d1 = RegInit(MixedVecInit((0 to conf.CSC_ATOMC-1) map { i => Fill(log2Ceil(i+2), false.B) }))
 
-    valid_d1 := io.input.valid
-    when(io.input.valid){
-        data_d1 := input_data_vec
-        mask_d1 := input_mask_vec
-        sel_d1 := input_sel_vec       
-    }
+//     valid_d1 := io.input.valid
+//     when(io.input.valid){
+//         data_d1 := input_data_vec
+//         mask_d1 := input_mask_vec
+//         sel_d1 := input_sel_vec       
+//     }
 
-    for(i <- 0 to conf.CSC_ATOMC-1){
-        when(io.input.valid & io.input_mask_en(i/8))
-        {
-            vec_sum_d1(i) := vec_sum(i)
-        }
-    }  
+//     for(i <- 0 to conf.CSC_ATOMC-1){
+//         when(io.input.valid & io.input_mask_en(i/8))
+//         {
+//             vec_sum_d1(i) := vec_sum(i)
+//         }
+//     }  
 
-    ////////////////////////////////// phase II: mux //////////////////////////////////
-    val vec_data = Wire(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
+//     ////////////////////////////////// phase II: mux //////////////////////////////////
+//     val vec_data = Wire(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
 
-    for(j <- 0 to conf.CSC_ATOMC-1){
-        vec_data(j) := MuxLookup(vec_sum_d1(j), 0.asUInt(conf.CSC_BPE.W),            
-        (0 to j) map { i => (i+1).U -> data_d1(i) }
-        )
-    }     
+//     for(j <- 0 to conf.CSC_ATOMC-1){
+//         vec_data(j) := MuxLookup(vec_sum_d1(j), 0.asUInt(conf.CSC_BPE.W),            
+//         (0 to j) map { i => (i+1).U -> data_d1(i) }
+//         )
+//     }     
 
-    ////////////////////////////////// phase II: registers //////////////////////////////////
-    val valid_d2 = RegInit(false.B)
-    val sel_d2 = RegInit(VecInit(Seq.fill(conf.CSC_ATOMK)(false.B)))
-    val vec_data_d2 = Reg(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
+//     ////////////////////////////////// phase II: registers //////////////////////////////////
+//     val valid_d2 = RegInit(false.B)
+//     val sel_d2 = RegInit(VecInit(Seq.fill(conf.CSC_ATOMK)(false.B)))
+//     val vec_data_d2 = Reg(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
 
-    valid_d2 := valid_d1
-    when(valid_d1){
-        sel_d2 := sel_d1
-    }
-    for(i <- 0 to conf.CSC_ATOMC-1){
-        when(valid_d1){
-            when(mask_d1(i)){
-                vec_data_d2(i) := vec_data(i)
-            }
-            .otherwise{
-                vec_data_d2(i) := 0.asUInt(conf.CSC_BPE.W)
-            }
+//     valid_d2 := valid_d1
+//     when(valid_d1){
+//         sel_d2 := sel_d1
+//     }
+//     for(i <- 0 to conf.CSC_ATOMC-1){
+//         when(valid_d1){
+//             when(mask_d1(i)){
+//                 vec_data_d2(i) := vec_data(i)
+//             }
+//             .otherwise{
+//                 vec_data_d2(i) := 0.asUInt(conf.CSC_BPE.W)
+//             }
                 
-        }
-    }     
+//         }
+//     }     
 
-    ////////////////////////////////// phase III: registers //////////////////////////////////
-    val mask_d2_int8_w = Wire(Vec(conf.CSC_ATOMC, Bool()))
-    for(i <- 0 to conf.CSC_ATOMC-1){
-        mask_d2_int8_w(i) := vec_data(i).asUInt.orR
-    }
+//     ////////////////////////////////// phase III: registers //////////////////////////////////
+//     val mask_d2_int8_w = Wire(Vec(conf.CSC_ATOMC, Bool()))
+//     for(i <- 0 to conf.CSC_ATOMC-1){
+//         mask_d2_int8_w(i) := vec_data(i).asUInt.orR
+//     }
 
-    val mask_d2_w = mask_d2_int8_w //only for int8
+//     val mask_d2_w = mask_d2_int8_w //only for int8
 
-    val valid_d3 = RegInit(false.B)
-    val mask_d3 = Reg(Vec(conf.CSC_ATOMC, Bool()))
-    val sel_d3 = RegInit(VecInit(Seq.fill(conf.CSC_ATOMK)(false.B)))
-    val vec_data_d3 = Reg(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
+//     val valid_d3 = RegInit(false.B)
+//     val mask_d3 = Reg(Vec(conf.CSC_ATOMC, Bool()))
+//     val sel_d3 = RegInit(VecInit(Seq.fill(conf.CSC_ATOMK)(false.B)))
+//     val vec_data_d3 = Reg(Vec(conf.CSC_ATOMC, UInt(conf.CSC_BPE.W)))
 
-    valid_d3 := valid_d2
-    when(valid_d2){
-        mask_d3 := mask_d2_w 
-        sel_d3 := sel_d2
-        vec_data_d3 := vec_data_d2
-    }
+//     valid_d3 := valid_d2
+//     when(valid_d2){
+//         mask_d3 := mask_d2_w 
+//         sel_d3 := sel_d2
+//         vec_data_d3 := vec_data_d2
+//     }
 
-    ////////////////////////////////// output: rename //////////////////////////////////    
-    io.output.valid := valid_d3
-    io.output.bits.mask := mask_d3.asUInt
-    io.output.bits.sel := sel_d3.asUInt
-    io.output.bits.data := vec_data_d3
+//     ////////////////////////////////// output: rename //////////////////////////////////    
+//     io.output.valid := valid_d3
+//     io.output.bits.mask := mask_d3.asUInt
+//     io.output.bits.sel := sel_d3.asUInt
+//     io.output.bits.data := vec_data_d3
     
-}}
+// }}
 
-object NV_NVDLA_CSC_WL_decDriver extends App {
-  implicit val conf: nvdlaConfig = new nvdlaConfig
-  chisel3.Driver.execute(args, () => new NV_NVDLA_CSC_WL_dec)
-}
+// object NV_NVDLA_CSC_WL_decDriver extends App {
+//   implicit val conf: nvdlaConfig = new nvdlaConfig
+//   chisel3.Driver.execute(args, () => new NV_NVDLA_CSC_WL_dec)
+// }
 
 
